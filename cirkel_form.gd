55eb648e7bc0d419da383751ff6ab416c671f4ef
camilla <milla23mn@gmail.com>
2025-03-24 12:56:22 +0100

    Gjort så det man kan trække en form hen til deres plads og de "snapper" til pladsen, og kan også fjernes igen.

--- cirkel_form.gd
+++ cirkel_form.gd
@@ -6,8 +6,8 @@ var of = Vector2(0,0)
 func _process(delta: float) -> void:
 	if dragging:
 		position = get_global_mouse_position() - of
-
-
+		
+		
 func _on_button_button_down() -> void:
 	dragging = true
 	of = get_global_mouse_position() - global_position
@@ -16,9 +16,10 @@ func _on_button_button_down() -> void:
 func _on_button_button_up() -> void:
 	dragging = false
 	check_snap()
+	
 
 func check_snap():
-	for area in get_tree().get_nodes_in_group("sort_trekant"): # Skift til den rigtige gruppe
+	for area in get_tree().get_nodes_in_group("sort_cirkel"): # Skift til den rigtige gruppe
 		if global_position.distance_to(area.global_position) < 80:  # Snap-afstand
 			global_position = area.global_position
 			return  # Stop efter første match
